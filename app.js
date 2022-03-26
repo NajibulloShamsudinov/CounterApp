6a0c69d47096352404cb06a15b9d4df222f2705b
Najibulloh Shamsudinov <najibullohshamsudinzoda@gmail.com>
2022-03-26 15:59:59 +0500

    Counter

---
+++ app.js
@@ -0,0 +1,103 @@
+    /* let a=6;
+    var b=5;
+    console.log("a + b = " , a+b)
+    console.log("a - b = " , a-b)
+    console.log("a * b = " , a*b)
+    console.log("a / b = " , a/b)
+    console.log("a % b = " , a%b)
+
+    console.log("++a = " , ++a)
+    console.log("a++ = " , a++)
+    console.log("a = " , a) 
+
+    console.log("--a = " , --a)
+    console.log("a-- = " , a--)
+    console.log('a = ' , a) 
+
+    console.log("a ** b = " , a ** b)
+
+
+    var result;
+    var opperator = '*';
+    var number1 = 5;
+    var number2 = 6;
+    switch(opperator){
+        case '+':
+        result = number1 + number2;
+        console.log(`${number1} + ${number2} = ${result}`) 
+        break;    
+        case '-':
+        result = number1 - number2;
+        console.log(`${number1} - ${number2} = ${result}`)    
+        break;
+        case '*':
+        result = number1 * number2;
+        console.log(`${number1} * ${number2} = ${result}`)    
+        break;
+    }
+
+
+
+
+
+    
+    let fruit ='apple';
+    switch(fruit){
+        case 'apple':
+        case 'mango':
+        case 'pineapple':
+        console.log(`${fruit} is a fruit.`);
+        break;
+        default:
+        console.log(`${fruit} is not fruit.`);
+        break;            
+    */
+    /*
+    for (let i=1; i<=5; i++){
+        if(i==3){continue}
+        console.log(i)
+    }
+    */
+
+
+
+    // let  a = "abcdefghijklmnopnjn";
+
+    /* 
+    for (let i=0; i<a.length; i++){
+        console.log(a[i])
+    }
+    */
+
+    /*
+    for (let k of a){
+        console.log(k)
+    }
+    */
+
+
+    //    DOM
+    /*
+    let element =document.getElementById("block")
+
+    element.addEventListener("click", function(){
+        element.innerHTML = "Hello Soft club"
+    })
+    */
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
